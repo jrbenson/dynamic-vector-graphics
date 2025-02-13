09b0c32f662ae7fdb20a56f918af79d36bf34cd6
Riley Benson <jordan.riley.benson@gmail.com>
2025-02-13 15:16:02 -0500

    Saving before switching to vite.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { DEFAULT_SYNTAX } from './syntax/syntax'
 import { parseSyntax } from './syntax/parse'
+import { encodeSyntax } from './syntax/encode'
 
 export { DVG } from './dvg'
 export { Data } from './data/data'
 
 export { RE_SYNTAXCONTAINER } from './syntax/syntax'
-export { JUSTIFICATIONS, ARRANGEMENTS, DIMENSIONS, COLUMN_TYPES, COLUMN_SYMBOLS, COMPARISONS } from './syntax/syntax'
-export { Syntax, DEFAULT_SYNTAX, DEFAULT_SYNTAX_COLUMN, DEFAULT_SYNTAX_CONDITION } from './syntax/syntax'
+export {
+  Syntax,
+  DEFAULT_SYNTAX,
+  DEFAULT_SYNTAX_COLUMN,
+  DEFAULT_SYNTAX_CONDITION,
+  JUSTIFICATIONS,
+  ARRANGEMENTS,
+  DIMENSIONS,
+  COLUMN_TYPES,
+  COLUMN_SYMBOLS,
+  COMPARISONS,
+} from './syntax/syntax'
 export { parseSyntax } from './syntax/parse'
+export { encodeSyntax } from './syntax/encode'
 export { Markup, parseMarkup, stringifyMarkup } from './syntax/markup'
 export { filter as parseFilter } from './syntax/markup'
